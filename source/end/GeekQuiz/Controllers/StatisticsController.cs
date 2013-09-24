ad3cb2cbb1c193617a868225655e0f82eeb582e6
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2013-09-24 20:08:00 -0300

    # Added WIP end solution

---
+++ source/end/GeekQuiz/Controllers/StatisticsController.cs
@@ -0,0 +1,39 @@
+﻿using GeekQuiz.Models;
+using GeekQuiz.Services;
+using GeekQuiz.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GeekQuiz.Controllers
+{
+    public class StatisticsController : ApiController
+    {
+        private TriviaContext db;
+        private StatisticsService statisticsService;
+
+        public StatisticsController()
+        {
+            this.db = new TriviaContext();
+            this.statisticsService = new StatisticsService(db);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this.db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public async Task<StatisticsViewModel> Get()
+        {
+            StatisticsViewModel statistics =
+                await this.statisticsService.GenerateStatistics();
+
+            return statistics;
+        }
+    }
+}
